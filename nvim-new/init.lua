636174b6f1ca769c1ae84d5d94dda793018bf0dd
nikola <nktaushanov@gmail.com>
2026-02-09 01:25:38 +0200

    Add lualine

--- nvim-new/init.lua
+++ nvim-new/init.lua
@@ -74,6 +74,16 @@ require("lazy").setup({
       },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = "catppuccin",
+      },
+    },
+  },
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
