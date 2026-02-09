0cc13893cec3207c7bcfd5ea471e9e33610b79fd
nikola <nktaushanov@gmail.com>
2026-02-09 02:11:44 +0200

    A bunch of nvim plugins

--- nvim-new/init.lua
+++ nvim-new/init.lua
@@ -89,6 +89,85 @@ require("lazy").setup({
     opts = {},
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup({
+        ensure_installed = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "typescript",
+          "tsx",
+          "javascript",
+          "python",
+          "bash",
+          "go",
+        },
+        highlight = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({})
+      telescope.load_extension("fzf")
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "ts_ls", "pyright", "bashls", "eslint" },
+      automatic_installation = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "mason-org/mason-lspconfig.nvim" },
+    config = function()
+      local servers = { "ts_ls", "pyright", "bashls", "gopls", "eslint" }
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local opts = { noremap = true, silent = true, buffer = event.buf }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<Leader>f", function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+        end,
+      })
+
+      vim.lsp.enable(servers)
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
+        callback = function()
+          pcall(vim.cmd, "EslintFixAll")
+        end,
+      })
+    end,
+  },
+  {
     "lewis6991/gitsigns.nvim",
     opts = {},
   },
@@ -119,6 +198,13 @@ vim.keymap.set("n", "<C-t>", ":tabnew<CR>", { noremap = true })
 vim.keymap.set("n", "<Leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { noremap = true })
 vim.keymap.set("n", "<Leader>gd", ":DiffviewOpen<CR>", { noremap = true })
 vim.keymap.set("n", "<Leader>gD", ":DiffviewClose<CR>", { noremap = true })
+
+vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>fg", ":Telescope live_grep<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>fb", ":Telescope buffers<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>fh", ":Telescope help_tags<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>fs", ":Telescope lsp_document_symbols<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>fS", ":Telescope lsp_workspace_symbols<CR>", { noremap = true })
 
 vim.o.langmap = vim.o.langmap
   .. "чявертъуиопшщасдфгхйклзьцжбнмЧЯВЕРТЪУИОПШЩАСДФГХЙКЛЗѝЦЖБНМ;`qwertyuiop[]asdfghjklzxcvbnm~QWERTYUIOP{}ASDFGHJKLZXCVBNM,ю\\,Ю\\|,"
